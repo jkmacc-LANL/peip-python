391b9b4731b3efba463c7d0412a50e86b75ffd23
Jonathan MacCarthy <jkmacc@lanl.gov>
2018-06-20 22:51:57 -0600

    Add MPL style file to peip package

---
+++ peip/__init__.py
@@ -0,0 +1,5 @@
+import os
+import matplotlib.pyplot as plt
+
+# use the 'bookfonts' matplotlib style throughout the package
+plt.style.use(os.path.dirname(__file__) + os.path.sep + 'bookfonts.mplstyle')
